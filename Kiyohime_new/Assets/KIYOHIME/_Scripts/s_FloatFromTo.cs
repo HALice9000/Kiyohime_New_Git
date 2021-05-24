d4f21000949fb7f6adb30d8dc2bc94051903b8ea
Maltiranis <73780448+HALice9000@users.noreply.github.com>
2021-05-24 19:00:42 +0200

    debug timeline + décor boss

---
+++ Kiyohime_new/Assets/KIYOHIME/_Scripts/s_FloatFromTo.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class s_FloatFromTo : MonoBehaviour {
+
+    float progress = 0;
+
+    public float GetProgression()
+    {
+        return progress;
+    }
+
+    public void FloatFromTo(float _from, float _to, float delay, float steps)
+    {
+        progress = _from;
+        StartCoroutine(FromToDelay(_from, _to, delay, steps));
+    }
+
+    IEnumerator FromToDelay(float f, float t, float d, float s)
+    {
+        yield return new WaitForSeconds(s * d);
+
+        progress += s;
+
+        if (progress <= t)
+        {
+            StartCoroutine(FromToDelay(progress, t, d, s));
+        }
+    }
+}
---
+++ Kiyohime_new/Assets/KIYOHIME/_Scripts/s_SpritesFader.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[ExecuteInEditMode]
+public class s_SpritesFader : MonoBehaviour
+{
+    [Range(0, 1)]
+    public float _alpha = 1f;
+    public float _delay = 1f;
+    public float _steps = 0.1f;
+
+    public SpriteRenderer[] sprites;
+
+    SpriteRenderer[] spriteRenderer;
+
+    public s_FloatFromTo FFT;
+
+    void FindSprites()
+    {
+        spriteRenderer = new SpriteRenderer[sprites.Length];
+
+        for (int i = 0; i < sprites.Length; i++)
+        {
+            spriteRenderer[i] = sprites[i].GetComponent<SpriteRenderer>();
+        }
+    }
+
+    private void OnEnable()
+    {
+        FindSprites();
+        FFT.FloatFromTo(0, 1, _delay, _steps);
+    }
+
+    void Update()
+    {
+        _alpha = FFT.GetProgression();
+        foreach (var sprite in spriteRenderer)
+        {
+            sprite.color = new Color(1, 1, 1, _alpha);
+        }
+    }
+}
