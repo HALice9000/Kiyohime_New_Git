0d61eb1edd5866f964a6528dd0f9fd36319c03fa
Maltiranis <73780448+HALice9000@users.noreply.github.com>
2021-05-26 01:09:18 +0200

    cascades sound design + ajout musique final
    ajout et modif avec lilian

---
+++ Kiyohime_new/Assets/KIYOHIME/_Scripts/final_sound.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class final_sound : MonoBehaviour
+{
+    public AK.Wwise.State musicState;
+
+    public void ChangeMusic()
+    {
+        musicState.SetValue();
+    }
+
+    public AK.Wwise.Event Respiration;
+    public void EventRespiration()
+    {
+        Respiration.Post(Squelette);
+
+
+    }
+
+   public GameObject Squelette;
+}
